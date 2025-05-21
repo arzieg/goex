fd09f7684da2337484883caaf7197c2dbae93a0f
Arne Ziegler <arne.ziegler@gmx.net>
2025-05-21 15:50:00 +0200

    [ADD] mutex

---
+++ concurrency/mutex1/mutex1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func do() int {
+	var m sync.Mutex
+	var n int64
+	var w sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		w.Add(1)
+
+		go func() {
+			m.Lock()
+			n++
+			m.Unlock()
+			w.Done()
+		}()
+	}
+	w.Wait()
+	return int(n)
+}
+
+func main() {
+	fmt.Println(do())
+}
